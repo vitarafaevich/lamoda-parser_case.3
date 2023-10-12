819f1ac69a34f4028c5830b229c8d260bfdd3578
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-10-12 22:32:37 +0700

    gg

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 Котлярова Полина
 Рафаевич Вита
 """
-
+import requests
+url = 'https://www.lamoda.ru/catalogsearch/result/?q=%D0%BA%D1%83%D1%80%D1%82%D0%BA%D0%B0%20%D0%B7%D0%B8%D0%BC%D0%BD%D1%8F%D1%8F%20%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F&&submit=y&page=1'
+r = requests.get(url)
+text = r.text
+print(text)
