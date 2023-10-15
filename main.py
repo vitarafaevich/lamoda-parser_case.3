280fe00a6745707af3751d96416c2774d5de41bb
Kotlyarova04 <125530657+Kotlyarova04@users.noreply.github.com>
2023-10-15 22:01:18 +0700

    main.py

--- main.py
+++ main.py
@@ -14,18 +14,16 @@ if __name__ == '__main__':
     text = response.text
 
     name_pattern = r'<div\s+class="x-product-card-description__product-name">(.*?)</div>'
-    price_pattern = r'<div\s+class="x-product-card-description__x-product-card__card">(.*?)</div>'
+    #price_pattern = r'<div\s+class="x-product-card-description__x-product-card__card">(.*?)</div>'
     brand_pattern = r'<div\s+class="x-product-card-description__brand-name">([^<]+)</div>'
 
     names = re.findall(name_pattern, text)
-    prices = re.findall(price_pattern, text)
+    #prices = re.findall(price_pattern, text)
     brands = re.findall(brand_pattern, text)
 
 
     for brand in brands:
         print(f'Бренд: {brand}')
-    for price in prices:
-        print(f'Бренд: {price}')
 
     for name, brand in zip(names, brands):
         print(f'Наименование:{name}, Бренд: {brand}')
