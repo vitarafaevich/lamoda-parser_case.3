f7938e4eed42d758fa34ddaf72383218dda46ece
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-10-16 22:46:04 +0700

    changes

--- main.py
+++ main.py
@@ -15,46 +15,50 @@ if __name__ == '__main__':
     text = response.text
     page_pattern = r'"pagination":{"page":\d+,"pages":(\d+),'
     pages = re.findall(page_pattern, text)
+    pages = str(pages).strip("[']")
+    for page in range(1, int(pages)+1):
+        params = {'q': goods, 'page': page}
+        response = requests.get('https://www.lamoda.ru/catalogsearch/result', params=params)
+        text = response.text
 
-    name_pattern = r'<div\s+class="x-product-card-description__product-name">(.*?)</div>'
-    price_pattern = r'span>\s+class="x-product-card-description__price-old[^"₽]*">([^<]+)</span>'
-    brand_pattern = r'<div\s+class="x-product-card-description__brand-name">([^<]+)</div>'
-    article_pattern = r'"short_sku":"([A-Za-z0-9]+)"'
+        name_pattern = r'<div\s+class="x-product-card-description__product-name">(.*?)</div>'
+        brand_pattern = r'<div\s+class="x-product-card-description__brand-name">([^<]+)</div>'
+        article_pattern = r'"short_sku":"([A-Za-z0-9]+)"'
 
-    articles = re.findall(article_pattern, text)
-    countries = []
-    prices = []
-    discounts = []
-    products = []
-    for article in articles:
-        product_url = f'{base_url}/p/{article}'
-        response = requests.get(product_url)
-        product = response.text
+        articles = re.findall(article_pattern, text)
+        countries = []
+        prices = []
+        discounts = []
+        products = []
+        for article in articles:
+            product_url = f'{base_url}/p/{article}'
+            response = requests.get(product_url)
+            product = response.text
 
-        country_pattern = r'"production_country","title":"Страна производства","value":"(.*?)"'
-        country = re.findall(country_pattern, product)
-        country = str(country).strip("[']")
-        countries.append(country)
+            country_pattern = r'"production_country","title":"Страна производства","value":"(.*?)"'
+            country = re.findall(country_pattern, product)
+            country = str(country).strip("[']")
+            countries.append(country)
 
-        price_pattern = r'"price":"(.*?)"'
-        pricess = re.findall(price_pattern, product)
-        prices.append(pricess[0])
+            price_pattern = r'"price":"(.*?)"'
+            pricess = re.findall(price_pattern, product)
+            prices.append(pricess[0])
 
-        pricess = str(pricess).replace(' ', '')
-        pricess = pricess.split(',')
+            pricess = str(pricess).replace(' ', '')
+            pricess = pricess.split(',')
 
-        if len(pricess) == 2:
-            pricess[0] = pricess[0].strip("[']")
-            pricess[1] = pricess[1].strip("[']")
-            discount = int(((int(pricess[1]) - int(pricess[0])) / int(pricess[1])) * 100)
-            discounts.append(f'{discount}%')
-        else:
-            discount = '0%'
-            discounts.append(discount)
+            if len(pricess) == 2:
+                pricess[0] = pricess[0].strip("[']")
+                pricess[1] = pricess[1].strip("[']")
+                discount = int(((int(pricess[1]) - int(pricess[0])) / int(pricess[1])) * 100)
+                discounts.append(f'{discount}%')
+            else:
+                discount = '0%'
+                discounts.append(discount)
 
-    names = re.findall(name_pattern, text)
-    brands = re.findall(brand_pattern, text)
+        names = re.findall(name_pattern, text)
+        brands = re.findall(brand_pattern, text)
 
-    for article, name, brand, price, discount, country in zip(articles, names, brands, prices, discounts, countries):
-        print(f'Number: {article}, Name: {name}, Brand: {brand}, Price: {price}, Discount: {discount}, 'f'Production Country: {country}')
+        for article, name, brand, price, discount, country in zip(articles, names, brands, prices, discounts, countries):
+            print(f'Number: {article}, Name: {name}, Brand: {brand}, Price: {price}, Discount: {discount}, 'f'Production Country: {country}')
 
